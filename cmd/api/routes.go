a343a3e90ad0edff0f18851868224f3fb1319fbc
Eduarte <74308809+EduartePaiva@users.noreply.github.com>
2026-06-09 10:23:50 -0300

    adding basic router

---
+++ cmd/api/routes.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/go-chi/chi/v5"
+)
+
+func (app *application) routes() http.Handler {
+	r := chi.NewRouter()
+
+	r.Get("/v1/healthcheck", app.healthcheckHandler)
+	r.Post("/v1/movies", app.createMovieHandler)
+	r.Get("/v1/movies/{id}", app.showMovieHandler)
+
+	return r
+}
