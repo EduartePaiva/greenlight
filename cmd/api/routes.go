733638c9b9f015c5e31e730a65f9363ecbcc3a9a
Eduarte <74308809+EduartePaiva@users.noreply.github.com>
2026-06-13 12:33:01 -0300

    json decoding

--- cmd/api/routes.go
+++ cmd/api/routes.go
@@ -10,7 +10,6 @@ func (app *application) routes() http.Handler {
 	r := chi.NewRouter()
 	r.NotFound(app.notFoundResponse)
 	r.MethodNotAllowed(app.methodNotAllowedResponse)
-	r.Use(app.recoverPanic)
 
 	r.Get("/v1/healthcheck", app.healthcheckHandler)
 	r.Post("/v1/movies", app.createMovieHandler)
