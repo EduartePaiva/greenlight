bb76c21405605955313dbdeef25967dfa36f504a
Eduarte <74308809+EduartePaiva@users.noreply.github.com>
2026-06-15 19:59:02 -0300

    creating custom unmarshal

--- internal/data/runtime.go
+++ internal/data/runtime.go
@@ -1,11 +1,15 @@
 package data
 
 import (
+	"errors"
 	"fmt"
 	"strconv"
+	"strings"
 )
 
 type Runtime int32
+
+var ErrInvalidRuntimeFormat = errors.New("invalid runtime format")
 
 // MarshalJSON implements the [encoding/json.Marshaler] interface.
 func (r Runtime) MarshalJSON() ([]byte, error) {
@@ -14,4 +18,25 @@ func (r Runtime) MarshalJSON() ([]byte, error) {
 	quotedJSONValue := strconv.Quote(jsonValue)
 
 	return []byte(quotedJSONValue), nil
+}
+
+// [encoding/json.Unmarshaler]
+func (r *Runtime) UnmarshalJSON(jsonValue []byte) error {
+	unquotedJSONValue, err := strconv.Unquote(string(jsonValue))
+	if err != nil {
+		return ErrInvalidRuntimeFormat
+	}
+
+	nStr, hasSuffix := strings.CutSuffix(unquotedJSONValue, " mins")
+	if !hasSuffix {
+		return ErrInvalidRuntimeFormat
+	}
+
+	n, err := strconv.ParseInt(nStr, 10, 32)
+	if err != nil {
+		return ErrInvalidRuntimeFormat
+	}
+	*r = Runtime(n)
+
+	return nil
 }
