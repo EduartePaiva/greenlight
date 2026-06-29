3aee9a47e5aa849ef7e8c79caec8f85ff682ba67
Eduarte <74308809+EduartePaiva@users.noreply.github.com>
2026-06-29 12:57:34 -0300

    implementing rate limit per client with automatic cleanup

--- cmd/api/errors.go
+++ cmd/api/errors.go
@@ -48,6 +48,11 @@ func (app *application) badRequestResponse(w http.ResponseWriter, r *http.Reques
 	app.errorResponse(w, r, http.StatusBadRequest, err.Error())
 }
 
+func (app *application) rateLimitExceededResponse(w http.ResponseWriter, r *http.Request) {
+	message := "rate limit exceeded"
+	app.errorResponse(w, r, http.StatusTooManyRequests, message)
+}
+
 func (app *application) failedValidationResponse(w http.ResponseWriter, r *http.Request, errors map[string]string) {
 	app.errorResponse(w, r, http.StatusUnprocessableEntity, errors)
 }
