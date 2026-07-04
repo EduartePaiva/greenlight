000c75c6ee6239f98f9ea38d656ee979b70aa220
Eduarte <74308809+EduartePaiva@users.noreply.github.com>
2026-07-04 12:24:42 -0300

    building database queries pt2

--- cmd/api/routes.go
+++ cmd/api/routes.go
@@ -18,6 +18,7 @@ func (app *application) routes() http.Handler {
 	r.MethodNotAllowed(app.methodNotAllowedResponse)
 
 	r.Get("/v1/healthcheck", app.healthcheckHandler)
+
 	r.Post("/v1/movies", app.createMovieHandler)
 	r.Get("/v1/movies", app.listMovieHandler)
 	r.Get("/v1/movies/{id}", app.showMovieHandler)
--- internal/data/users.go
+++ internal/data/users.go
@@ -107,9 +107,56 @@ func (u UserModel) Insert(user *User) error {
 }
 
 func (u UserModel) GetByEmail(email string) (*User, error) {
-	return nil, nil
+	query := `
+		SELECT id, created_at, name, email, password_hash, activated, version
+		FROM users
+		WHERE email = $1`
+
+	var user User
+
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second*3)
+	defer cancel()
+
+	err := u.DB.QueryRowContext(ctx, query, email).Scan(
+		&user.ID,
+		&user.CreatedAt,
+		&user.Name,
+		&user.Email,
+		&user.Password.hash,
+		&user.Activated,
+		&user.Version,
+	)
+	if errors.Is(err, sql.ErrNoRows) {
+		return nil, ErrRecordNotFound
+	} else if err != nil {
+		return nil, err
+	}
+
+	return &user, nil
 }
 
 func (u UserModel) Update(user *User) error {
+	query := `
+		UPDATE users 
+		SET name = $1, email = $2, password_hash = $3, activated = $4, version = version + 1
+		WHERE id = $5 AND version = $6
+		RETURNING version`
+
+	args := []any{user.Name, user.Email, user.Password.hash, user.Activated, user.ID, user.Version}
+
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second*3)
+	defer cancel()
+
+	err := u.DB.QueryRowContext(ctx, query, args...).Scan(&user.Version)
+	if err != nil {
+		switch {
+		case errors.Is(err, sql.ErrNoRows):
+			return ErrRecordNotFound
+		case err.Error() == `pq: duplicate key value violates unique constraint "users_email_key"`:
+			return ErrDuplicateEmail
+		default:
+			return err
+		}
+	}
 	return nil
 }
