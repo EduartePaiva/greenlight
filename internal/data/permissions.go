ae1cc4baa64564740b02062a95d6b931367aba02
Eduarte <74308809+EduartePaiva@users.noreply.github.com>
2026-07-14 09:10:23 -0300

    setting up permissions sql tables and models

---
+++ internal/data/permissions.go
@@ -0,0 +1,51 @@
+package data
+
+import (
+	"context"
+	"database/sql"
+	"slices"
+	"time"
+)
+
+type Permissions []string
+
+func (p Permissions) Include(code string) bool {
+	return slices.Contains(p, code)
+}
+
+type PermissionModel struct {
+	DB *sql.DB
+}
+
+func (m PermissionModel) GetAllForUser(userID int64) (Permissions, error) {
+	query := `SELECT permissions.code
+		FROM users_permissions
+		INNER JOIN permissions ON permissions.id = users_permissions.permission_id
+		WHERE user_id = $1`
+
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second*3)
+	defer cancel()
+
+	rows, err := m.DB.QueryContext(ctx, query, userID)
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
+
+	var permissions Permissions
+
+	for rows.Next() {
+		var permission string
+		err := rows.Scan(&permission)
+		if err != nil {
+			return nil, err
+		}
+		permissions = append(permissions, permission)
+	}
+
+	if err = rows.Err(); err != nil {
+		return nil, err
+	}
+
+	return permissions, nil
+}
