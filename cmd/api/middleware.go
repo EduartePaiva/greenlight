b3f764a9e343f538a488925d7db6f72c98ba2659
Eduarte <74308809+EduartePaiva@users.noreply.github.com>
2026-07-18 10:03:25 -0300

    handling preflight cors requests

--- cmd/api/middleware.go
+++ cmd/api/middleware.go
@@ -177,11 +177,21 @@ func (app *application) requirePermission(permission string) func(http.Handler) 
 func (app *application) EnableCORS(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Add("Vary", "Origin")
+		w.Header().Add("Vary", "Access-Control-Request-Method")
 
 		origin := r.Header.Get("Origin")
 
 		if origin != "" && validator.PermittedValue(origin, app.config.cors.trustedOrigins...) {
 			w.Header().Set("Access-Control-Allow-Origin", origin)
+
+			if r.Method == http.MethodOptions && r.Header.Get("Access-Control-Request-Method") != "" {
+				w.Header().Add("Access-Control-Allow-Methods", "OPTIONS, PUT, PATCH, DELETE")
+				w.Header().Add("Access-Control-Allow-Headers", "Authorization, Content-Type")
+				w.Header().Add("Access-Control-Max-Age", "1800")
+
+				w.WriteHeader(http.StatusOK)
+				return
+			}
 		}
 
 		next.ServeHTTP(w, r)
